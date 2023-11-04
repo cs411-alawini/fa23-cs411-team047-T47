e98cc8ff6d0df51e574bf9935ffcaf6d5ad94a07
Wei Wu <128621069+weiw-w@users.noreply.github.com>
2023-11-04 18:35:49 -0500

    added close by routes function

--- ConnectionDB.py
+++ ConnectionDB.py
@@ -7,9 +7,6 @@ def getCloseByStops(lat, lon):
 
   mycursor = db.cursor()
 
-  # sql = ("CALL `closeByStop`({},{});".format(lat,lon))
-  # # address = (lat,lon)
-
   # mycursor.execute(sql)
 
   mycursor.callproc('closeByStop',[lat,lon])
@@ -17,15 +14,23 @@ def getCloseByStops(lat, lon):
   for result in mycursor.stored_results():
     details = result.fetchall()
 
-  # myresult = mycursor.fetchall()
+  for det in details:
+    print(det)
+
+def getCloseByRoutes(lat, lon):
+  db = mysql.connector.connect(user='root', password='cs411t47db',
+                                host='34.28.132.25',
+                                database='cs411')
+
+  mycursor = db.cursor()
+
+  mycursor.callproc('closeByRoutes',[lat,lon])
+
+  for result in mycursor.stored_results():
+    details = result.fetchall()
 
   for det in details:
     print(det)
 
-  
-  # for x in myresult:
-  #   print(x)
-
-  # print (len(myresult))
-
-getCloseByStops(-23.432174,-46.787095)
+# getCloseByStops(-23.432174,-46.787095)
+getCloseByRoutes(-23.432174,-46.787095)
