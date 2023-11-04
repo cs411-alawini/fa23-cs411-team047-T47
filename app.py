7b4fdea859c79321e25cdfebf9b469444665d5e8
tychenNTU <68041127+tychenNTU@users.noreply.github.com>
2023-11-04 02:37:25 -0500

    Update app.py with new changes

--- app.py
+++ app.py
@@ -1,10 +1,58 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Assuming you have set up Cloud SQL with a public IP and have whitelisted your server's IP to access it.
+# Replace the placeholders with your actual credentials.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://<username>:<password>@<host>/<database>'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Database model for stops
+class BusStop(db.Model):
+    __tablename__ = 'bus_stops'  # Ensure this matches your actual table name in MySQL
+    stop_id = db.Column(db.Integer, primary_key=True)
+    stop_name = db.Column(db.String(256), nullable=False)
+    stop_desc = db.Column(db.String(256), nullable=True)
+    stop_lat = db.Column(db.Float, nullable=False)
+    stop_lon = db.Column(db.Float, nullable=False)
+
+# Geocoding API setup
+GEOCODING_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
+GEOCODING_API_KEY = 'AIzaSyBS-S37L58YssF52HQocELjBEI4s1-NSiM'
+
+@app.route('/geocode', methods=['POST'])
+def geocode_address():
+    data = request.get_json()
+    address = data.get('address')
+
+    # Geocode the address using the Google Geocoding API
+    response = requests.get(GEOCODING_API_URL, params={
+        'address': address,
+        'key': GEOCODING_API_KEY
+    })
+
+    if response.status_code != 200:
+        return jsonify({'error': 'Geocoding API error'}), response.status_code
+
+    geocoding_data = response.json()
+    if geocoding_data['status'] != 'OK':
+        return jsonify({'error': 'Invalid address'}), 400
+
+    # Extract latitude and longitude
+    location = geocoding_data['results'][0]['geometry']['location']
+    latitude = location['lat']
+    longitude = location['lng']
+
+    # At this point, you can send back the latitude and longitude to the frontend
+    # or if you need to save this in the database, you can do so here.
+
+    return jsonify({
+        'latitude': latitude,
+        'longitude': longitude
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
