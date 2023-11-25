b056645baf3b9cf94b8d915e995c176e1068338d
Wei Wu <128621069+weiw-w@users.noreply.github.com>
2023-11-25 00:53:38 -0600

    Created the function for getting the schedule array

--- ConnectionDB.py
+++ ConnectionDB.py
@@ -1,5 +1,7 @@
 import mysql.connector
 import math
+from datetime import datetime
+import numpy as np
 
 # Get the new coordinates of the 400 meter boundary of given address
 def calculate_new_coordinates(lat, lon, bearing_degrees):
@@ -59,10 +61,11 @@ def get_close_by_stops(lat, lon):
 
   # for det in details:
   #   print(det)
-
-  return details
+  
   mycursor.close()
   db.close()
+
+  return details
 
 # Get the close by routes within 400 meter circle of given address
 def get_close_by_routes(lat, lon):
@@ -77,10 +80,60 @@ def get_close_by_routes(lat, lon):
   for result in mycursor.stored_results():
     details = result.fetchall()
 
+  mycursor.close()
+  db.close()
+
   return details
+
+
+# Get the whole schedule for a route toward a direction (trip)
+def get_schedule(trip_id):
+  db = mysql.connector.connect(user='root', password='cs411t47db',
+                                host='34.28.132.25',
+                                database='cs411')
+
+  mycursor = db.cursor()
+  
+  # To get the number of freqency per this trip
+  result = mycursor.callproc('frequency',[trip_id, 0])
+  freq_num = result[1]
+
+  # To get the number of stops per this trip
+  result = mycursor.callproc('stopNum',[trip_id, 0])
+  stop_num = result[1]
+
+  schedule = np.zeros([freq_num + 1,stop_num])
+  schedule = schedule.astype('str') 
+
+  f_count = 0
+
+  s_count = 0
+
+  # To get the list of stop names in order
+  mycursor.callproc('stopName',[trip_id])
+
+  for result in mycursor.stored_results():
+    details = result.fetchall()
+
+  for det in details:
+    schedule[0][s_count] = det[0]
+    s_count += 1
+
+  mycursor.callproc('schedule',[trip_id])
+
+  # To put the schedule data in 
+  for result in mycursor.stored_results():
+    details = result.fetchall()
+
+  for det in details:
+    if (det[1] == 1):
+      f_count += 1
+    schedule[f_count,det[1]-1] = str(det[0])
 
   mycursor.close()
   db.close()
 
+  return schedule
+
 # get_close_by_routes(-23.439609, -46.807039)
-# get_close_by_stops(-23.439609, -46.807039)
+# get_schedule('1012-10-0')
